557451789583197e2e5576ab666c93b33519199a
Felix Strobel <62568203+felixstrobel@users.noreply.github.com>
2024-12-07 23:10:03 +0100

    New Client with ClientOptions

--- mailtm.go
+++ mailtm.go
@@ -1,20 +1,74 @@
 package mailtm
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 )
 
 type Service string
 
 const (
-	MAIL_TM Service = "https://api.mail.tm"
+	DefaultBaseURL string = "https://api.mail.tm"
 )
 
-type MailClient struct {
+type Client struct {
 	http    *http.Client
-	service Service
+	baseUrl string
+	token   string
 }
 
-func New() (*MailClient, error) {
-	return &MailClient{service: MAIL_TM, http: &http.Client{}}, nil
+type ClientOption func(*Client)
+
+func WithBaseURL(url string) ClientOption {
+	return func(client *Client) {
+		client.baseUrl = url
+	}
+}
+
+func WithHttpClient(httpClient *http.Client) ClientOption {
+	return func(client *Client) {
+		client.http = httpClient
+	}
+}
+
+func New(opts ...ClientOption) *Client {
+	httpClient := &http.Client{}
+
+	client := &Client{
+		http:    httpClient,
+		baseUrl: DefaultBaseURL,
+	}
+
+	for _, opt := range opts {
+		opt(client)
+	}
+
+	return client
+}
+
+func (c *Client) authenticatedGetRequest(req *http.Request, result interface{}) error {
+	if len(c.token) == 0 {
+		return errors.New("missing authentication")
+	}
+
+	req.Header.Set("Authorization", "Bearer "+c.token)
+
+	return c.request(req, result)
+}
+
+func (c *Client) request(req *http.Request, result interface{}) error {
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	res, err := c.http.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	//TODO: check status codes
+
+	return json.NewDecoder(res.Body).Decode(result)
 }
